37598f3f5bafd91682e6d5e54d6df2a6271a71b6
Dirk Reich <dirkforpresident@gmail.com>
2026-08-21 14:53:28 +0200

    v1.3.1: Repeater-Positionen aus Adverts korrekt (1e6), Namen ohne Binärmüll

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "sh.mapme.mapper"
         minSdk = 26
         targetSdk = 35
-        versionCode = 103
-        versionName = "1.3.0"
+        versionCode = 104
+        versionName = "1.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
